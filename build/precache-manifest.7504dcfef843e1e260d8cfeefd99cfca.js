3f5692a748242e31517b8a12447d89a850b53948
Imfortes <imfortes@list.ru>
2020-03-09 19:21:25 +0300

    add sort

---
+++ build/precache-manifest.7504dcfef843e1e260d8cfeefd99cfca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77161cf448731f9b468055f3d2d835f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "271246c6bc53d310acf8",
+    "url": "/static/css/main.1f350c3f.chunk.css"
+  },
+  {
+    "revision": "56a00875272d734fab18",
+    "url": "/static/js/2.a80a76fe.chunk.js"
+  },
+  {
+    "revision": "3b173ed738774b4bb64ac2b5ddb03f3b",
+    "url": "/static/js/2.a80a76fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "271246c6bc53d310acf8",
+    "url": "/static/js/main.75fd34f2.chunk.js"
+  },
+  {
+    "revision": "f86499479480d0d527b0",
+    "url": "/static/js/runtime-main.1677fdde.js"
+  }
+]);
